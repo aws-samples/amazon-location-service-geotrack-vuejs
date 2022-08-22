8db0463e5c256c5c0dc04200dbda947d9696f145
Awais Abdul Khaliq <awais.a.khaliq@gmail.com>
2022-08-22 19:19:38 +0500

    Disabled config load for babel

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,11 +8,11 @@ module.exports = {
       'eslint:recommended'
     ],
     parserOptions: {
-      parser: '@babel/eslint-parser'
+      parser: '@babel/eslint-parser',
+      "requireConfigFile": false
     },
     rules: {
       'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-      'requireConfigFile': false,
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
     }
   }
